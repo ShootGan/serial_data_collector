28909da67abbeaf673e6149cb1417342b8e3c5a5
ShootGan <nawelpalepka@gmail.com>
2022-04-12 22:56:52 +0200

    added tests and some logic

--- tests/test_serial_settings.py
+++ tests/test_serial_settings.py
@@ -2,6 +2,7 @@ import sys
 import glob
 import serial
 import pytest
+import random
 from src.Core.serial_setttings import SerialSettings
 
 
@@ -32,8 +33,29 @@ class TestSerialSettings:
         get_ports_list = [x for x in test_port_setting.ports.keys()]
         assert get_ports_list == system_ports
 
+    def test_update_bitratet(self):
+        """tests update bitrate to typical values """
+        defult_value = 9600
+        typical_bitrate_values = (1200, 2400, 4800, 9600, 115200)
+        test_object = SerialSettings()
+        assert test_object.current_bitrate == defult_value
+        for birate in typical_bitrate_values:
+            test_object.update_bitrate(birate)
+            assert test_object.current_bitrate == birate
 
+    def test_update_bitratet_too_big(self):
+        """tests update bitrate to too big value"""
+        value_bigger_than_limit = 256001
+        test_object = SerialSettings()
+        with pytest.raises(Exception):
+            test_object.update_bitrate(value_bigger_than_limit)
+        assert test_object.current_bitrate is not value_bigger_than_limit
 
-
-
-
+    def test_update_port(self):
+        """tests if current port can be correctly updated"""
+        mock_ports_dict = {"com1": "desc1", "com2": "desc2", "com3": "desc1"}
+        random_port = random.choice(list(mock_ports_dict))
+        test_object = SerialSettings()
+        test_object.ports = mock_ports_dict
+        test_object.update_port(random_port)
+        assert test_object.current_port == random_port
