ca8bdf967e27f0ff5f00da782f54fd0a1719ca41
ShootGan <nawelpalepka@gmail.com>
2022-04-13 20:10:02 +0200

    update tests for linux

--- tests/test_serial_settings.py
+++ tests/test_serial_settings.py
@@ -13,13 +13,8 @@ class TestSerialSettings:
         """compare cmd or bash serial port list with serial port list from _get_ports method"""
         if sys.platform.startswith('win'):
             ports = ['COM%s' % (i + 1) for i in range(256)]
-        elif sys.platform.startswith('linux') or sys.platform.startswith('cygwin'):
-            # this excludes your current terminal "/dev/tty"
-            ports = glob.glob('/dev/tty[A-Za-z]*')
-        elif sys.platform.startswith('darwin'):
-            ports = glob.glob('/dev/tty.*')
         else:
-            raise EnvironmentError('Unsupported platform')
+            pytest.skip("skipping windows-only tests")
 
         system_ports = []
         for port in ports:
